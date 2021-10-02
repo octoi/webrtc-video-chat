86f6d42de62f9bfe3ffc038dfd3ca0c1ee5cbb20
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-03 04:47:17 +0530

    socket io

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,13 @@
 var MyApp = (function () {
-  function init(uid, mid) {}
+  function init(uid, mid) {
+    event_process_for_signaling_server();
+  }
+
+  var socket = null;
+  function event_process_for_signaling_server() {
+    socket = io.connect();
+    socket.on('connect', () => alert('socket connected to server'));
+  }
 
   return {
     _init: function (uid, mid) {
