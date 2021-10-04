dc8656ceb2bf1a61ef7703c4eb285d92091e40cd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-05 04:30:02 +0530

    i quit :(

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,90 @@
+var AppProcess = function () {
+  async function _init(SDP_function, connId) {}
+
+  var iceConfig = {
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun1.l.google.com:19302',
+      },
+    ],
+  };
+
+  async function setConnection(connId) {
+    var connection = new RTCPeerConnection(iceConfig);
+
+    connection.onnegotiationneeded = async function (event) {
+      await setOffer(connId);
+    };
+    connection.onicecandidate = function (event) {
+      if (event.candidate) {
+        serverProcess(
+          JSON.stringify({ iceCandidate: event.candidate }),
+          connId
+        );
+      }
+    };
+  }
+
+  return {
+    setNewConnection: async function (connId) {
+      await setConnection(connId);
+    },
+    init: async function (SDP_function, connId) {
+      await _init(SDP_function, connId);
+    },
+  };
+};
+
 var MyApp = (function () {
+  var socket = null;
+  var user_id = '';
+  var meeting_id = '';
+
   function init(uid, mid) {
+    user_id = uid;
+    meeting_id = mid;
     event_process_for_signaling_server();
   }
 
-  var socket = null;
   function event_process_for_signaling_server() {
     socket = io.connect();
-    socket.on('connect', () => alert('socket connected to server'));
+
+    var SDP_function = function (data, connId) {
+      socket.emit('SDPProcess', {
+        message: data,
+        to_connId: to_connId,
+      });
+    };
+
+    socket.on('connect', () => {
+      if (socket.connected) {
+        AppProcess.init(SDP_function, socket.id);
+        if (user_id != '' && meeting_id != '') {
+          socket.emit('userconnect', {
+            displayName: user_id,
+            meeting_id: meeting_id,
+          });
+        }
+      }
+    });
+
+    socket.on('inform_others_about_me', (data) => {
+      addUser(data.other_user_id, data.connId);
+      AppProcess.setNewConnection(data.connId);
+    });
+
+    function addUser(other_user_id, connId) {
+      var newDivId = $('#otherTemplate').clone();
+      newDivId = newDivId.attr('id', connId).addClass('other');
+      newDivId.find('h2').text = other_user_id;
+      newDivId.find('video').attr('id', `v_${connId}`);
+      newDivId.find('audio').attr('id', `a_${connId}`);
+      newDivId.show();
+      $('#divUsers').append(newDivId);
+    }
   }
 
   return {
--- server.js
+++ server.js
@@ -11,6 +11,29 @@ const io = require('socket.io')(server, {
 });
 app.use(express.static(path.join(__dirname, '')));
 
+var userConnections = [];
+
 io.on('connection', (socket) => {
   console.log('socket id is ', socket.id);
+
+  socket.on('userconnect', (data) => {
+    console.log('user connect: ', data.displayName, data.meeting_id);
+
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meeting_id
+    );
+
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meeting_id,
+    });
+
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit('inform_others_about_me', {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+  });
 });
